25da0465de3c338537707ac47b41af881669b618
Myrxu <mar.botwina@gmail.com>
2019-02-23 12:20:06 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    /**
+     * moja pierwsza lekcja
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Mariusz Botwina");
+        // To jest moje imie oraz nazwiko
+        System.out.println("Witaj");
+        System.out.println("Mariusz");
+        System.out.println("Botwina");
+        /*wyzej napisalem kolejno
+        a) powitanie
+        b) Imie
+        c) Nazwisko
+         */
+        int liczba1 = 58;
+        double liczba2 = 198273;
+        double liczba3 = liczba1 + liczba2;
+
+        System.out.println(liczba3);
+
+        char symbol1 = 'a';
+        int symbol2 = symbol1;
+        System.out.println("symbol2 = " + symbol2);
+    }
+}
